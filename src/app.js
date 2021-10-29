ddd802faca787a4065761e18fce05f30196a19e8
Haisem Jemal <haisemjemal@Haisems-MacBook-Pro.local>
2021-10-29 17:24:02 -0400

    finished adding js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
-// Wrapped in IFFE to prevent any namespace errors
-(function () {
-    console.log('Test js');
-})()
+const modal = document.getElementById('modal')
+const modalOutcome = document.getElementById('modalOutcome')
+
+/**
+ * Function clears previous content in modal outcome & displays the modal
+ */
+function modalTrigger() {
+    modal.style.display = "flex";
+    modalOutcome.innerHTML = " ";
+}
+
+/**
+ * Function responsible for closing the modal and loading the approriate content into modal outcome div
+ * userChoice is passed through, determined by which button the user clicks
+ */
+function userAlert(userChoice) {
+    modal.style.display = "none";
+    modalOutcome.style.display = "flex";
+    modalOutcome.innerHTML = userChoice
+}
